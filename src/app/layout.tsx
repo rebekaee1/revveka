29ad11bb33b153bef9a187c45ff83ac0847f65c3
iprofi <iprofi@MacBook-Pro-iprofi.local>
2025-12-19 12:47:31 +0300

    Fix: Яндекс Метрика - inline script в head для корректной работы

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Script from 'next/script';
 import './globals.css';
 
 const inter = Inter({
@@ -60,31 +59,32 @@ export default function RootLayout({
     <html lang="ru" className={inter.variable}>
       <head>
         {/* Yandex.Metrika counter - ID: 105920123 */}
-        <Script id="yandex-metrika" strategy="afterInteractive">
-          {`
-            (function(m,e,t,r,i,k,a){
-              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {
-                if (document.scripts[j].src === r) { return; }
-              }
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document, 'script', 'https://mc.webvisor.org/metrika/tag_ww.js?id=105920123', 'ym');
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                  if (document.scripts[j].src === r) { return; }
+                }
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document, 'script', 'https://mc.webvisor.org/metrika/tag_ww.js?id=105920123', 'ym');
 
-            ym(105920123, 'init', {
-              ssr: true,
-              webvisor: true,
-              trackHash: true,
-              clickmap: true,
-              ecommerce: 'dataLayer',
-              accurateTrackBounce: true,
-              trackLinks: true
-            });
-          `}
-        </Script>
+              ym(105920123, 'init', {
+                ssr: true,
+                webvisor: true,
+                trackHash: true,
+                clickmap: true,
+                ecommerce: 'dataLayer',
+                accurateTrackBounce: true,
+                trackLinks: true
+              });
+            `,
+          }}
+        />
         <noscript>
           <div>
-            {/* eslint-disable-next-line @next/next/no-img-element */}
             <img 
               src="https://mc.yandex.ru/watch/105920123" 
               style={{ position: 'absolute', left: '-9999px' }} 
@@ -92,24 +92,6 @@ export default function RootLayout({
             />
           </div>
         </noscript>
-        
-        {/* Google Analytics placeholder - set NEXT_PUBLIC_GA_ID to enable */}
-        {process.env.NEXT_PUBLIC_GA_ID && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-              `}
-            </Script>
-          </>
-        )}
       </head>
       <body className="font-sans antialiased bg-white text-slate-900">
         {children}
