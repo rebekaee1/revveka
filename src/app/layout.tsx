157b1a94b6b271c9fd16d52fbe24f3533e67146c
iprofi <iprofi@MacBook-Pro-iprofi.local>
2025-12-19 12:56:51 +0300

    Fix: Яндекс Метрика - правильный URL tag_ww.js и компактный inline код

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,38 +58,15 @@ export default function RootLayout({
   return (
     <html lang="ru" className={inter.variable}>
       <head>
-        {/* Yandex.Metrika counter - ID: 105920123 */}
+        {/* Yandex.Metrika counter */}
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-              (function(m,e,t,r,i,k,a){
-                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-                for (var j = 0; j < document.scripts.length; j++) {
-                  if (document.scripts[j].src === r) { return; }
-                }
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-              })(window, document, 'script', 'https://mc.webvisor.org/metrika/tag_ww.js?id=105920123', 'ym');
-
-              ym(105920123, 'init', {
-                ssr: true,
-                webvisor: true,
-                trackHash: true,
-                clickmap: true,
-                ecommerce: 'dataLayer',
-                accurateTrackBounce: true,
-                trackLinks: true
-              });
-            `,
+            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.webvisor.org/metrika/tag_ww.js?id=105920123", "ym");ym(105920123, "init", {clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true,trackHash:true,ecommerce:"dataLayer"});`,
           }}
         />
         <noscript>
           <div>
-            <img 
-              src="https://mc.yandex.ru/watch/105920123" 
-              style={{ position: 'absolute', left: '-9999px' }} 
-              alt="" 
-            />
+            <img src="https://mc.yandex.ru/watch/105920123" style={{ position: 'absolute', left: '-9999px' }} alt="" />
           </div>
         </noscript>
       </head>
